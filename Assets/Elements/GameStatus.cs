9f071d98d21bcf3d100766c0655aeb5c47d3a653
Doods <yusufalishabaka@tutanota.com>
2026-08-27 16:05:37 +0300

    Spawn an enemy every second

--- Assets/Elements/GameStatus.cs
+++ Assets/Elements/GameStatus.cs
@@ -5,9 +5,28 @@ public class GameStatus : MonoBehaviour
     public static GameStatus instance;
 
     public Transform playerTransform;
+    public Object enemy;
 
     public void Awake()
     {
         instance = this;
+        InvokeRepeating(nameof(SpawnEnemy), 3.0f, 1.0f);
     }
+
+    void SpawnEnemy()
+    {
+        static float RR()
+        {
+            float x = 0f;
+            while (x == 0)
+            {
+                x = Random.Range(-1f, 1f);
+            }
+
+            return x;
+        }
+        Vector3 offset = new Vector3(RR(), RR(), 0).normalized * Random.Range(20f, 40f);
+        Instantiate(enemy, playerTransform.position + offset, Quaternion.identity);
+    }
+
 }
--- Assets/Enemy/EnemyController.cs
+++ Assets/Enemy/EnemyController.cs
@@ -19,12 +19,9 @@ public class EnemyController : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 playerPosition = GameStatus.instance.playerTransform.position;
-        Vector3 position = this.transform.position;
+        Vector3 position = transform.position;
         Vector3 moveDirection = (playerPosition - position).normalized;
 
-        print(moveDirection * speed);
-
-        this.transform.position += moveDirection * speed * Time.deltaTime;
-        // this.transform.position += new Vector3(100, 100, 0);
+        transform.position += moveDirection * speed * Time.deltaTime;
     }
 }
