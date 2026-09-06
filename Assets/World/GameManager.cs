339f908007bb85469eb8196aa04deb459b5c4666
TheToastedIron <ahmed3881146@gmail.com>
2026-09-06 18:38:31 +0300

    Fixed a line to follow the format

--- Assets/World/GameManager.cs
+++ Assets/World/GameManager.cs
@@ -156,7 +156,7 @@ public class GameManager : MonoBehaviour
             static float RR()
             {
                 float x = 0f;
-                while (x == 0) { x = Random.Range(-1f, 1f); }
+                while (x == 0) x = Random.Range(-1f, 1f);
                 return x;
             }
             Vector3 offset = new Vector3(RR(), RR(), 0).normalized * Random.Range(20f, 40f);
