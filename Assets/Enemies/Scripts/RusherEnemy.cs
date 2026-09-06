e8ab88480007c9f4d7fbd7184255b9f1ec2989a7
Doods <yusufalishabaka@tutanota.com>
2026-09-06 23:30:04 +0300

    Add rusher enemy

---
+++ Assets/Enemies/Scripts/RusherEnemy.cs
@@ -0,0 +1,29 @@
+public class RusherEnemy : EnemyController
+{
+    public float startRushRange;
+    public float endRushRange;
+    public float acceleration;
+    bool rushing = false;
+
+    public override void FixedUpdate()
+    {
+        float distanceToPlayer = ToPlayer().magnitude;
+
+        if (distanceToPlayer >= endRushRange) rushing = false;
+        else if (distanceToPlayer <= startRushRange && !rushing)
+        {
+            rushing = true;
+            rigidbody.linearVelocity = ToPlayer().normalized * speed;
+        }
+
+        if (rushing) rigidbody.linearVelocity *= 1 + acceleration / 1000;
+        else if (rigidbody.linearVelocity.magnitude > speed * 1.1)
+        {
+            rigidbody.linearVelocity /= 1 + acceleration / 100;
+        }
+        else rigidbody.linearVelocity = ToPlayer().normalized * speed;
+
+
+        ReactToKnockback();
+    }
+}
