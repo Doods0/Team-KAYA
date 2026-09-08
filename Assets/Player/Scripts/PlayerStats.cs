8e8cb061278d73737f6bef58bc140edeb95a0817
Doods <yusufalishabaka@tutanota.com>
2026-09-08 12:20:01 +0300

    Do not start a shockwave if no damage is taken
    This prevents the player from abusing 0 damage attacks, like the archer's melee.

--- Assets/Player/Scripts/PlayerStats.cs
+++ Assets/Player/Scripts/PlayerStats.cs
@@ -98,7 +98,7 @@ public class PlayerStats : MonoBehaviour
 
     public void TakeDamage(int damageTaken)
     {
-        if (isImmune) return;
+        if (isImmune || damageTaken == 0) return;
 
         health = Mathf.Clamp(health - damageTaken, 0, 100);
 
