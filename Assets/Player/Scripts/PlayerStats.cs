ef6647b18299a620c4eb96cadf0b5b590271e1f4
Doods <yusufalishabaka@tutanota.com>
2026-09-08 12:24:32 +0300

    Clamp displayed HP to maxHealth instead of 100

--- Assets/Player/Scripts/PlayerStats.cs
+++ Assets/Player/Scripts/PlayerStats.cs
@@ -107,7 +107,7 @@ public class PlayerStats : MonoBehaviour
     {
         if (isImmune) return;
 
-        health = Mathf.Clamp(health - damageTaken, 0, 100);
+        health = Mathf.Clamp(health - damageTaken, 0, maxHealth);
 
         HUD.UpdateHealth(health, maxHealth);
 
