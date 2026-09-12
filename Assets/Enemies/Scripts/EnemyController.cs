feb25627463e272215b3111d8223f6e8de738680
Doods <80251531+Doods0@users.noreply.github.com>
2026-09-12 11:05:49 +0300

    Update Assets/Enemies/Scripts/EnemyController.cs

--- Assets/Enemies/Scripts/EnemyController.cs
+++ Assets/Enemies/Scripts/EnemyController.cs
@@ -86,7 +86,7 @@ public class EnemyController : MonoBehaviour
         return speed * GameManager.instance.timeScale;
     }
 
-    private void handleLoot()
+    private void HandleLoot()
     {
         PickupController controller;
         GameObject pickupObj;
