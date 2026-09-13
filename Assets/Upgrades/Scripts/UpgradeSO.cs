21f7bdc02c75d3e54379d654d1a275619d1d5732
TheToastedIron <ahmed3881146@gmail.com>
2026-09-14 01:30:18 +0300

    Set up upgrades from the player's side

--- Assets/Upgrades/Scripts/UpgradeSO.cs
+++ Assets/Upgrades/Scripts/UpgradeSO.cs
@@ -1,21 +1,25 @@
 using UnityEngine;
 
+public enum PlayerStatsIncreaseType { Addition, Percentage }
+// weapons are always a percentage that stacks up with upgrades by addition
+public enum Frequency { Once, Update } // Will this upgrade increase or decrease a stat once or link two stats together
+
 public class UpgradeSO : ScriptableObject
 {
-    public enum IncreaseType { Addition, Percentage }
-    public enum Frequency { Once, Update }
     // Update would require a reference of the functional PlayerStats though
     // And a custom function too
     // Example : SpeedCorrespondToKnockback
+    // ApplyEffect just takes the active playerStats and links two values with each other or something idk
 
-    // Upgrade name
-    // Upgrade texture
-    // Upgrade increase Type
-    // Upgrade frequency
+    [Header("Basic Data")]
+    public string title;
+    public string description;
+    public Sprite icon;
+    public PlayerStatsIncreaseType increaseType;
+    public Frequency frequency;
 
     // The other stats are determined by other SOs inheriting as this script won't be usable on its own
-
-    public virtual void ApplyEffect(PlayerStats activeStats = null) { }
+    public virtual void ApplyEffect(PlayerStats activePlayerStats, WeaponsBuffs activeWeaponBuffs) { }
     // Add to it the two other detached classes representing light and heavy weapons' stats
 
 }
