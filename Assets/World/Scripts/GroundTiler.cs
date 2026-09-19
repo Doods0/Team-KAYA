a9f38213900bdfda29e08b59761d9180ba01264e
TheToastedIron <ahmed3881146@gmail.com>
2026-09-19 15:16:59 +0300

    Added infinitely tileable ground (#74)

---
+++ Assets/World/Scripts/GroundTiler.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class GroundTiler : MonoBehaviour
+{
+    [Header("Target & Setup")]
+    [SerializeField] private Transform[] tiles = new Transform[9];
+
+    [Header("Settings")]
+    [SerializeField] private Vector2 tileSize;
+
+    private void LateUpdate()
+    {
+        Vector3 player = GameUtils.instance.playerPosition;
+
+        // Check each tile individually
+        foreach (Transform tile in tiles)
+        {
+            if (tile == null) continue;
+
+            float deltaX = player.x - tile.position.x;
+            float deltaY = player.y - tile.position.y;
+
+            if (Mathf.Abs(deltaX) >= tileSize.x * 1.5f)
+            {
+                float moveX = Mathf.Sign(deltaX) * tileSize.x * 3f;
+                tile.position += new Vector3(moveX, 0f, 0f);
+            }
+
+            if (Mathf.Abs(deltaY) >= tileSize.y * 1.5f)
+            {
+                float moveY = Mathf.Sign(deltaY) * tileSize.y * 3f;
+                tile.position += new Vector3(0f, moveY, 0f);
+            }
+        }
+    }
+}
